4a774c4fe2a1a354374673379d34138d70e43f0e
José Manuel Oyola González <j.oyola@optima.local>
2025-04-08 18:05:15 +0200

    New spetialized node

---
+++ test/TreeVFlowWFormTest/GroupItemNode.cs
@@ -0,0 +1,28 @@
+﻿using TreeVFlowControl.Imp;
+
+namespace TreeVFlowWFormTest;
+
+public class GroupItemNode:TreeVFlowNode
+{
+    private bool _isNodeEnabled;
+
+    public GroupItemNode()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        
+    }
+    public void DisableNode()
+    {
+        if (!_isNodeEnabled) return;
+        
+    }
+    
+    public void EnableNode()
+    {
+        if (_isNodeEnabled) return;
+    }
+}
